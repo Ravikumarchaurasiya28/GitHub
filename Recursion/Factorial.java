f3652a21d2c1b69f532417b869c844fc83406211
Ravikumarchaurasiya28 <ravichaurasiya824@gmail.com>
2025-04-30 16:39:25 +0530

    fact

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class Sum_Natural {
-    static int recSum(int n) {
-        if(n == 1) return 1;
-        else return n + recSum(n - 1);
+public class Factorial {
+    static int fact(int n) {
+        if(n == 0 || n == 1) return 1;
+        else return  n * fact(n - 1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = sc.nextInt();
-        System.out.println("Sum: " + recSum(n));
+        System.out.println("Factorial: " + fact(n));
     }
 }
