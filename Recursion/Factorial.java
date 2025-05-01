fdb1200d37b1027cda23f8303e018b70744a44ab
Ravikumarchaurasiya28 <ravichaurasiya824@gmail.com>
2025-05-01 23:06:46 +0530

    fact

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Factorial {
     static int fact(int n) {
         if(n == 0 || n == 1) return 1;
-        else return  n * fact(n - 1);
+        else return n * fact(n - 1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = sc.nextInt();
-        System.out.println("Factorial: " + fact(n));
+        System.out.print("Factorial: " + fact(n));
     }
 }
